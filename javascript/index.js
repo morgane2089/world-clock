69615f671a0f85d744724a9d76a51391cbef8a0c
morgane2089 <93168158+morgane2089@users.noreply.github.com>
2024-04-11 16:04:38 +1000

    Added 3rd city Tokyo

--- javascript/index.js
+++ javascript/index.js
@@ -24,6 +24,19 @@ sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
 sydneyTimeElement.innerHTML = sydneyTime.format("h:mm:ss [<small>] A [</small>]");
 }
 
+// Tokyo
+
+let tokyoElement = document.querySelector("#tokyo") ;
+if (tokyoElement) {
+let tokyoDateElement = tokyoElement.querySelector(".date");
+let tokyoTimeElement = tokyoElement.querySelector(".time");
+let tokyoTime = moment().tz("Asia/Tokyo");
+
+
+tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>] A [</small>]");
+}
+
 };
 
 
